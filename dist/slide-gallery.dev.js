b77edca88dbc21481e5772e1ae7d0864d30a251c
James Cooper <cooper.jm@outlook.com>
2021-04-20 17:17:57 -0400

    Start of project

---
+++ dist/slide-gallery.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var galleryOptions = {
+  tabs: {
+    topTabs: true,
+    bottomTabs: false,
+    tabLabels: ['2000', '20001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2011', '2012']
+  }
+};
+slideGallery(galleryOptions);
+
+function slideGallery(options) {
+  // TODO get slide gallery container
+  var galleryContainer = document.getElementById('slide-gallery');
+  var tabs = options.tabs; //
+  //
+  // TODO check if tabs are needed, if needed create tabs container then get label array and create tab for each label and append to tabs container,then append container to slide gallery
+}
