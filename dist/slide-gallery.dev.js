5896d2cd56fb28e64e5cd571820c78d548aa7b8d
cooperjm <cooper.jm@outlook.com>
2021-04-21 16:32:01 -0400

    Populate Tabs and tab click events

--- dist/slide-gallery.dev.js
+++ dist/slide-gallery.dev.js
@@ -1,47 +1,65 @@
 "use strict";
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 var galleryOptions = {
-  tabs: _defineProperty({
+  tabs: {
     topTabs: true,
     bottomTabs: false,
-    tabLabels: true
-  }, "tabLabels", ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2011", "2012"])
+    tabLabels: ['2000', //
+    '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2011', '2012']
+  }
 };
-slideGallery(galleryOptions);
+slideGallery('slide-gallery', galleryOptions);
 
-function slideGallery(options) {
+function slideGallery(container, options) {
   // TODO get slide gallery container
-  var galleryContainer = document.getElementById("slide-gallery");
-  var tabs = options.tabs;
-  console.log(tabs); //
+  var main = {
+    container: document.getElementById(container),
+    tabs: options.tabs
+  };
+  populateGallery(); // Build the tabs and their containers and append to main container
+
+  function createTabs() {
+    var container = document.createElement('DIV');
+
+    if (main.tabs.topTabs) {
+      container.id = 'top-tabs';
+      container.classList.add('tabs');
+    }
+
+    var ul = document.createElement('UL');
+
+    if (main.tabs.tabLabels) {
+      main.tabs.tabLabels.forEach(function (tab) {
+        var daTab = document.createElement('LI');
+        daTab.classList.add('tabs__tab');
+        daTab.innerHTML = tab;
+        ul.appendChild(daTab);
+      });
+    }
+
+    container.appendChild(ul);
+    main.tabContainer = container;
+    main.container.appendChild(main.tabContainer); // Click event for tabs to scroll
+
+    ul.childNodes.forEach(function (li) {
+      li.addEventListener('click', function (event) {
+        var clicked = event.target.offsetLeft - ul.clientWidth / 2 + event.target.clientWidth / 2;
+        console.log(clicked);
+        main.tabContainer.scrollTo({
+          left: clicked,
+          behavior: 'smooth'
+        });
+      });
+    });
+  }
+
+  function populateGallery() {
+    createTabs();
+  } //
   //
   // TODO
   // check if tabs are needed, if needed create tabs container then get
   // label array and create tab for each label and append to tabs
   // container,then append container to slide gallery
 
-  createTabs(tabs);
-
-  function createTabs(tabs) {
-    var tabContainer = document.createElement("DIV");
-    tabContainer.id = "top-tabs";
-    tabContainer.classList.add("tabs");
-    var ul = document.createElement("UL");
-    var tabCounter = 1;
-
-    if (tabs.topTabs && tabs.tabLabels) {
-      tabs.tabLabels.forEach(function (tab) {
-        var singleTab = document.createElement("LI");
-        singleTab.classList.add("tabs__tab", "tab" + tabCounter);
-        tabCounter++;
-        singleTab.innerHTML = tab;
-        ul.appendChild(singleTab);
-      });
-    }
-
-    tabContainer.appendChild(ul);
-    galleryContainer.appendChild(tabContainer);
-  }
 }
--- slide-gallery.js
+++ slide-gallery.js
@@ -2,55 +2,76 @@ let galleryOptions = {
 	tabs: {
 		topTabs: true,
 		bottomTabs: false,
-		tabLabels: true,
 		tabLabels: [
-			"2000",
-			"2001",
-			"2002",
-			"2003",
-			"2004",
-			"2005",
-			"2006",
-			"2007",
-			"2008",
-			"2009",
-			"2011",
-			"2012",
+			'2000', //
+			'2001',
+			'2002',
+			'2003',
+			'2004',
+			'2005',
+			'2006',
+			'2007',
+			'2008',
+			'2009',
+			'2011',
+			'2012',
 		],
 	},
 };
 
-slideGallery(galleryOptions);
+slideGallery('slide-gallery', galleryOptions);
 
-function slideGallery(options) {
+function slideGallery(container, options) {
 	// TODO get slide gallery container
-	const galleryContainer = document.getElementById("slide-gallery");
-	const tabs = options.tabs;
-	console.log(tabs);
+	const main = {
+		container: document.getElementById(container),
+		tabs: options.tabs,
+	};
+
+	populateGallery();
+
+	// Build the tabs and their containers and append to main container
+	function createTabs() {
+		const container = document.createElement('DIV');
+		if (main.tabs.topTabs) {
+			container.id = 'top-tabs';
+			container.classList.add('tabs');
+		}
+
+		const ul = document.createElement('UL');
+		if (main.tabs.tabLabels) {
+			main.tabs.tabLabels.forEach((tab) => {
+				let daTab = document.createElement('LI');
+				daTab.classList.add('tabs__tab');
+				daTab.innerHTML = tab;
+				ul.appendChild(daTab);
+			});
+		}
+		container.appendChild(ul);
+		main.tabContainer = container;
+		main.container.appendChild(main.tabContainer);
+
+		// Click event for tabs to scroll
+		ul.childNodes.forEach((li) => {
+			li.addEventListener('click', (event) => {
+				const clicked = event.target.offsetLeft - ul.clientWidth / 2 + event.target.clientWidth / 2;
+				console.log(clicked);
+
+				main.tabContainer.scrollTo({
+					left: clicked,
+					behavior: 'smooth',
+				});
+			});
+		});
+	}
+
+	function populateGallery() {
+		createTabs();
+	}
 	//
 	//
 	// TODO
 	// check if tabs are needed, if needed create tabs container then get
 	// label array and create tab for each label and append to tabs
 	// container,then append container to slide gallery
-
-	createTabs(tabs);
-	function createTabs(tabs) {
-		const tabContainer = document.createElement("DIV");
-		tabContainer.id = "top-tabs";
-		tabContainer.classList.add("tabs");
-		const ul = document.createElement("UL");
-		let tabCounter = 1;
-		if (tabs.topTabs && tabs.tabLabels) {
-			tabs.tabLabels.forEach((tab) => {
-				const singleTab = document.createElement("LI");
-				singleTab.classList.add("tabs__tab", "tab" + tabCounter);
-				tabCounter++;
-				singleTab.innerHTML = tab;
-				ul.appendChild(singleTab);
-			});
-		}
-		tabContainer.appendChild(ul);
-		galleryContainer.appendChild(tabContainer);
-	}
 }
