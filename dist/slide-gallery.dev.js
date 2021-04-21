5ed00773a131a5a1805f16b5f628149c3a8f0b19
cooperjm <cooper.jm@outlook.com>
2021-04-20 21:54:22 -0400

    createTabs

--- dist/slide-gallery.dev.js
+++ dist/slide-gallery.dev.js
@@ -1,18 +1,47 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var galleryOptions = {
-  tabs: {
+  tabs: _defineProperty({
     topTabs: true,
     bottomTabs: false,
-    tabLabels: ['2000', '20001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2011', '2012']
-  }
+    tabLabels: true
+  }, "tabLabels", ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2011", "2012"])
 };
 slideGallery(galleryOptions);
 
 function slideGallery(options) {
   // TODO get slide gallery container
-  var galleryContainer = document.getElementById('slide-gallery');
-  var tabs = options.tabs; //
+  var galleryContainer = document.getElementById("slide-gallery");
+  var tabs = options.tabs;
+  console.log(tabs); //
   //
-  // TODO check if tabs are needed, if needed create tabs container then get label array and create tab for each label and append to tabs container,then append container to slide gallery
+  // TODO
+  // check if tabs are needed, if needed create tabs container then get
+  // label array and create tab for each label and append to tabs
+  // container,then append container to slide gallery
+
+  createTabs(tabs);
+
+  function createTabs(tabs) {
+    var tabContainer = document.createElement("DIV");
+    tabContainer.id = "top-tabs";
+    tabContainer.classList.add("tabs");
+    var ul = document.createElement("UL");
+    var tabCounter = 1;
+
+    if (tabs.topTabs && tabs.tabLabels) {
+      tabs.tabLabels.forEach(function (tab) {
+        var singleTab = document.createElement("LI");
+        singleTab.classList.add("tabs__tab", "tab" + tabCounter);
+        tabCounter++;
+        singleTab.innerHTML = tab;
+        ul.appendChild(singleTab);
+      });
+    }
+
+    tabContainer.appendChild(ul);
+    galleryContainer.appendChild(tabContainer);
+  }
 }
--- slide-gallery.js
+++ slide-gallery.js
@@ -2,7 +2,21 @@ let galleryOptions = {
 	tabs: {
 		topTabs: true,
 		bottomTabs: false,
-		tabLabels: ['2000', '20001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2011', '2012'],
+		tabLabels: true,
+		tabLabels: [
+			"2000",
+			"2001",
+			"2002",
+			"2003",
+			"2004",
+			"2005",
+			"2006",
+			"2007",
+			"2008",
+			"2009",
+			"2011",
+			"2012",
+		],
 	},
 };
 
@@ -10,9 +24,33 @@ slideGallery(galleryOptions);
 
 function slideGallery(options) {
 	// TODO get slide gallery container
-	const galleryContainer = document.getElementById('slide-gallery');
+	const galleryContainer = document.getElementById("slide-gallery");
 	const tabs = options.tabs;
+	console.log(tabs);
 	//
 	//
-	// TODO check if tabs are needed, if needed create tabs container then get label array and create tab for each label and append to tabs container,then append container to slide gallery
+	// TODO
+	// check if tabs are needed, if needed create tabs container then get
+	// label array and create tab for each label and append to tabs
+	// container,then append container to slide gallery
+
+	createTabs(tabs);
+	function createTabs(tabs) {
+		const tabContainer = document.createElement("DIV");
+		tabContainer.id = "top-tabs";
+		tabContainer.classList.add("tabs");
+		const ul = document.createElement("UL");
+		let tabCounter = 1;
+		if (tabs.topTabs && tabs.tabLabels) {
+			tabs.tabLabels.forEach((tab) => {
+				const singleTab = document.createElement("LI");
+				singleTab.classList.add("tabs__tab", "tab" + tabCounter);
+				tabCounter++;
+				singleTab.innerHTML = tab;
+				ul.appendChild(singleTab);
+			});
+		}
+		tabContainer.appendChild(ul);
+		galleryContainer.appendChild(tabContainer);
+	}
 }
