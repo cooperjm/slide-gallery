c13285a6f2171abb188301e693b590c79a19b9c2
cooperjm <cooper.jm@outlook.com>
2021-04-26 17:00:24 -0400

    Restarting the Javascript

--- dist/slide-gallery.dev.js
+++ dist/slide-gallery.dev.js
@@ -1,1 +1,42 @@
 "use strict";
+
+var gOptions = {
+  images: [['https://placedog.net/900/600?random', 'Test Alt Text', 'Test Copy'], // URL, alt text, copy text
+  ['https://placedog.net/901/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/902/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/903/600?random', 'Test Alt Text', ''], ['https://placedog.net/904/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/905/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/906/600?random', 'Test Alt Text', ''], ['https://placedog.net/907/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/908/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/909/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/910/600?random', 'Test Alt Text', 'Test Copy'], ['https://placedog.net/911/600?random', 'Test Alt Text', '']],
+  arrowControls: true,
+  tabNav: 'none',
+  // top, bottom, or none
+  tabNavLabels: [],
+  // Must have same amount as images
+  thumbnails: true // Uses the url from images
+
+}; // console.log(gOptions);
+
+slideGallery('slide-gallery', gOptions);
+
+function slideGallery(container, options) {
+  var gallery = {
+    galleryRoot: document.getElementById(container)
+  };
+  createGalleryElements();
+
+  function createGalleryElements() {
+    // Create Tabs Container
+    var tabs = document.createElement('DIV');
+    tabs.id = 'tabs-container';
+    tabs.classList.add('tabs');
+    gallery.tabs = tabs; // Create Images Container
+
+    var galleryContainer = document.createElement('DIV');
+    galleryContainer.id = 'gallery-container';
+    var imagesContainer = document.createElement('DIV');
+    imagesContainer.classList.add('images-container');
+    galleryContainer.appendChild(imagesContainer);
+    gallery.galleryContainer = galleryContainer;
+  }
+
+  function createGalleryComponents() {} // const galleryHolder = document.getElementById(container);
+
+
+  console.log(gallery);
+}
